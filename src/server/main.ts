2dafdac390fd5561b2af68a130c5615164d02fa6
pollovolante <64164316+pollovolante@users.noreply.github.com>
2022-09-26 17:38:21 +0200

    Update main.ts

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,7 +2,6 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 import {ServiceHandler} from "./services/ServiceHandler"
-import {DatabaseService} from "./services/database/DatabaseService"
 import {ExpressWebServerService} from "./services/WebServer/WebServer"
 
 const handler = new ServiceHandler()
@@ -12,10 +11,6 @@ handler.setMany([
     key:'ExpressWebServer',
     service:WebServerService
   },
-  {
-    key:'DataStore',
-    service:new DatabaseService(process.env.MONGODB_LINK!)
-  }
 ])
 handler.initALL()
 
